fa83b4a9fa2c69f3591f4027f737be74de77d90a
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-03-08 11:51:55 -0500

    get delete, dont delete dogs man they are good

--- index.js
+++ index.js
@@ -22,10 +22,65 @@ app.use(
   })
 );
 
+const cats = [{
+  imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+},
+{
+  imageURL: 'https://media.licdn.com/dms/image/C4D03AQHZ-6yyqVB_mA/profile-displayphoto-shrink_800_800/0?e=1557360000&v=beta&t=03_sR7NHm_oHgyV-dRjHQ_gtVsQzMZKZjkoJPRRDaI4',
+  imageDescription: `Look hes chillen!`,
+  name: 'Peter',
+  sex: 'Male',
+  age: 6,
+  breed: 'NorEaster',
+  story: 'Thinkful grad'
+},
+];
+
+const dogs = [{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+},
+{
+  imageURL: 'https://media.licdn.com/dms/image/C5103AQEv9ADgSV2xOg/profile-displayphoto-shrink_800_800/0?e=1557360000&v=beta&t=nCEDVVxrTlsr2_6qEcGkjgzPEMh2w3m6a6G2j7Gl0SY',
+  imageDescription: `He's a smiler!`,
+  name: 'John',
+  sex: 'Male',
+  age: 5,
+  breed: 'Marylander',
+  story: 'Thinkful grad'
+}
+];
+
 app.get(`/api/cat`, (req, res, next) => {
-  res.send('Sup, Mikey');
-  }
+  const [cat] = cats;
+  res.send(cat);
+}
 );
+
+app.get('/api/dog', (req, res, next) => {
+  res.send(dogs[0]);
+});
+
+app.delete('/api/cat', (req, res, next) => {
+  cats.shift();
+  res.status(204).send();
+});
+
+app.delete('/api/dog', (req, res, next) => {
+  dogs.shift();
+  res.status(204).send();
+});
 
 function runServer(port = PORT) {
   const server = app
